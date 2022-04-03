cf7f98da9fc456128c3a6840db2ba5f54623717d
aratar79 <aratar79@gmail.com>
2022-04-03 21:49:07 +0200

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+
+// PrimeNG
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ButtonModule,
+    CardModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
